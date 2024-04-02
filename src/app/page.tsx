5f31c04fecde08e4b0cc54ac7a3e5ddc7c14b933
Andrew Casarsa <acasarsa@gmail.com>
2024-04-02 19:06:58 -0400

    rework to transcribe function, adding result manipulation, console logs for now. And some reorg + a few linting fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 'use client'
+import { deepgramTranscribe } from '@/services/actions/transcribe'
 import React, { useState } from 'react'
 
-import handleDeepgramFetch from '@/hooks/useDeepgramFetch'
 const url = 'https://dpgr.am/spacewalk.wav'
 
 export default function Home() {
   const [link, setLink] = useState(url)
+  console.log('link:', link)
 
   const handleLink = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setLink(event.target.value)
   }
 
-  const handleTranscribe = (): void => {
-    handleDeepgramFetch(url)
+  async function handleTranscribe() {
+    await deepgramTranscribe(url).then((res) => {
+      console.log('response data=', res!.data)
+    })
   }
 
   return (
