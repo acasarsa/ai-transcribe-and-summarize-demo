84031c212d4da92f40cef61e12388c13f1501dfc
Andrew Casarsa <acasarsa@gmail.com>
2024-04-02 16:12:19 -0400

    spelling and linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-"use client"
-import React, {useState} from 'react'
-
+'use client'
+import React, { useState } from 'react'
 
 import handleDeepgramFetch from '@/hooks/useDeepgramFetch'
 const url = 'https://dpgr.am/spacewalk.wav'
 
 export default function Home() {
-  const [link, setLink] = useState(url);
+  const [link, setLink] = useState(url)
 
   const handleLink = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setLink(event.target.value)
-  };
+  }
 
   const handleTranscribe = (): void => {
     handleDeepgramFetch(url)
@@ -18,8 +17,8 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <input value={link} onChange={handleLink} style={{color:'red'}}/>
-      <button onClick={handleTranscribe}>Transribe</button>
+      <input value={link} onChange={handleLink} style={{ color: 'red' }} />
+      <button onClick={handleTranscribe}>Transcribe</button>
     </main>
   )
 }
